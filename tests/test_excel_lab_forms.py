561916f7ec83b5ee6d385dbb689dfd7e2d976b1d
CoolMage <savva.arutsev@gmail.com>
2026-09-14 18:48:40 +0200

    Read lab template fixtures as UTF-8 on Windows

--- tests/test_excel_lab_forms.py
+++ tests/test_excel_lab_forms.py
@@ -14,7 +14,7 @@ ROOT = Path(__file__).parents[1] / 'examples/lab_forms_excel'
 @pytest.mark.parametrize('assay', ['von_frey', 'plantar', 'staircase'])
 @pytest.mark.parametrize('rats', [5, 10, 15, 20, 30, 40])
 def test_excel_print_fits_all_animals_and_rules(assay, rats):
-    template = TableTemplate.from_dict(json.loads((ROOT / 'templates' / f'{assay}.json').read_text()))
+    template = TableTemplate.from_dict(json.loads((ROOT / 'templates' / f'{assay}.json').read_text(encoding='utf-8')))
     original = template.to_dict()
     image = load_document(ROOT / 'fixtures' / f'{assay}_{rats}.pdf')[0]
     fitted = fit_template(template, detect_grid(image), image.shape[1] / image.shape[0])
